395008aacccb735c1d6607133684d24e3badcd79
Rubayz <rubayzaf2007@gmail.com>
2021-08-19 15:13:01 +0600

    base done!

---
+++ SlashCommands/client/ping.js
@@ -0,0 +1,15 @@
+const { Client, CommandInteraction } = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    description: "returns websocket ping",
+    type: 'CHAT_INPUT',
+    /**
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async(client, interaction, args) => {
+        interaction.followUp({ content: `${client.ws.ping} ms!` });
+    }
+}
